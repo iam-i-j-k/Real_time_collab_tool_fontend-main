123e8c90a164ef202d0cdd0f2085f3ed071fd5ba
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-03-23 15:42:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap
+import App from './App';
+
+// Create the necessary HTML structure directly in JavaScript
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
